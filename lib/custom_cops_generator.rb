e93ea6b815411cf6339992b7c94408e9035731ed
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-09-03 22:33:56 +0900

    bunlde gem

---
+++ lib/custom_cops_generator.rb
@@ -0,0 +1,6 @@
+require "custom_cops_generator/version"
+
+module CustomCopsGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/custom_cops_generator/version.rb
@@ -0,0 +1,3 @@
+module CustomCopsGenerator
+  VERSION = "0.1.0"
+end
