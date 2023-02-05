2826a91581f3ccd776eb0f34ef6d8a1575fd7648
Sam Bostock <sam.bostock@shopify.com>
2023-02-04 19:36:11 -0500

    Patch missing `require 'yaml'` in file using YAML

--- lib/rubocop/extension/generator/generator.rb
+++ lib/rubocop/extension/generator/generator.rb
@@ -85,6 +85,11 @@ module RuboCop
                - lib/#{name}.rb
           YML
 
+          patch "lib/#{dirname}.rb", %r{^require.*/version.$}, <<~RUBY.chomp
+            \\0
+            require 'yaml'
+          RUBY
+
           patch "lib/#{dirname}.rb", /^  end\nend/, <<~RUBY
                 PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
                 CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
