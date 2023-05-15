8f9686ba23eb5364b32b9db28c5ae4a972f02913
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2023-05-15 20:13:00 -0300

    fix uninitialized constant YAML when running ./bin/console

--- lib/rubocop/extension/generator/generator.rb
+++ lib/rubocop/extension/generator/generator.rb
@@ -85,6 +85,12 @@ module RuboCop
                - lib/#{name}.rb
           YML
 
+          patch "lib/#{dirname}.rb", /^# frozen_string_literal: true\n/, <<~RUBY
+          # frozen_string_literal: true
+
+          require 'yaml'
+          RUBY
+
           patch "lib/#{dirname}.rb", /^  end\nend/, <<~RUBY
                 PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
                 CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
